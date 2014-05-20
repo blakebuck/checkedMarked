8797a54dc07c286ad2d739b7544d99bc41138f18
Blake Buck <blakebuck@users.noreply.github.com>
2014-05-19 21:16:40 -0400

    Update checkedMarked.js
    Added functionality for already checked checkboxes and fixed problem of one checkbox checking them all.

--- checkedMarked.js
+++ checkedMarked.js
@@ -1,35 +1,51 @@
-(function( $ ) { 
-    var toggleCheckBox = function () {
+(function ($) {
+    var toggleCheckBox = function (chkbox) {
         // Toggle checkmark image
-        $(".checkMarked").toggleClass("checked");
+        $(chkbox).toggleClass("checked");
         // Toggle checkbox checked status.
-        $(".checkMarked input").prop('checked', function (i, value) {
+        $(chkbox).find("input[type='checkbox']").prop('checked', function (i, value) {
             return !value;
         });
     };
 
-    $.fn.checkedMarked = function() {
-        // Get checkbox's parent (used later to tell if chkbox is inside label)
-        chkbxParent = this.parent();
+    $.fn.checkedMarked = function () {
+
+        // Go through each checkbox individually.
+        this.filter( "input[type='checkbox']" ).each(function(){
+            // Get checkbox's parent (used to tell if chkbox is inside label)           
+            chkboxParent = $(this).parent();
+
+            // Check to see if we are inside of a label.
+            if (chkboxParent[0].nodeName.toLowerCase() == "label") {
+                // The parent element is a label
+                $(chkboxParent).addClass("checkedMarkedlabel");
+            }
+            
+            // Create new checkbox and hide old one
+            $(this).wrap("<span class='checkMarked'>&nbsp</span>");
+            $(".checkMarked input[type='checkbox']").hide();
+            
+            // Check to see if checkbox is already checked
+            if ($(this).attr("checked")) {
+                toggleCheckBox($(this).closest(".checkMarked"));
+            }
+        });                
         
-        // Create new checkbox and hide old one
-        this.wrap("<span class='checkMarked'>&nbsp</span>");
-        $(".checkMarked input").hide();
-        
-        // Check to see if we are inside of a label.
-        if (chkbxParent[0].nodeName.toLowerCase()=="label"){
-            // The parent element is a label
-            $(chkbxParent).addClass("checkedMarkedlabel");
-            $(".checkedMarkedlabel").click(function(event){
+        // Handle Label Click               
+        $(".checkedMarkedlabel").click(function (event) {
             // Without this we would get double click
-                event.preventDefault();
-                toggleCheckBox();        
-            });        
-        }
-        else {
-            // Parent is not a label
-            $(".checkMarked").click(toggleCheckBox);
-        }        
-        return this; 
+            event.preventDefault();
+            chkbox = $(this).find(".checkMarked");
+            toggleCheckBox(chkbox);
+        });
+        
+        // Handle Actual Checkbox Click
+        $(".checkMarked").click(function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            toggleCheckBox(this);
+        });
+
+        return this;
     };
 })(jQuery);
