f74137e524f96b58ac68e9fff96c285392a195c1
Blake Buck <blakebuck@users.noreply.github.com>
2014-08-23 16:43:43 -0400

    Prevent Double Binding

--- checkedMarked.js
+++ checkedMarked.js
@@ -13,38 +13,48 @@
         // Go through each checkbox individually.
         this.filter( "input[type='checkbox']" ).each(function(){
             // Get checkbox's parent (used to tell if chkbox is inside label)           
-            chkboxParent = $(this).parent();
+            var chkboxParent = $(this).parent();
 
             // Check to see if we are inside of a label.
             if (chkboxParent[0].nodeName.toLowerCase() == "label") {
                 // The parent element is a label
                 $(chkboxParent).addClass("checkedMarkedlabel");
+            }            
+            if ($(this).parent().hasClass("checkMarked")){                
+                $(this).unwrap("<span></span>");
             }
             
             // Create new checkbox and hide old one
-            $(this).wrap("<span class='checkMarked'>&nbsp</span>");
+            $(this).wrap("<span class='checkMarked'></span>");
             $(".checkMarked input[type='checkbox']").addClass("sr-only");
             
             // Check to see if checkbox is already checked
             if ($(this).attr("checked")) {
                 $(this).closest(".checkMarked").toggleClass("checked");
             }
-        });                
-        
-        // Handle Label Click               
-        $(".checkedMarkedlabel").click(function (event) {
+        });
+
+        labelClick = function(event){
             // Without this we would get double click
             event.preventDefault();
-            chkbox = $(this).find(".checkMarked");
+            var chkbox = $(this).find(".checkMarked");
             toggleCheckBox(chkbox);
-        });
-        
-        // Handle Actual Checkbox Click
-        $(".checkMarked").click(function (event) {
+        };
+        checkedMarkedClick = function(event){
             event.preventDefault();
             event.stopPropagation();
             toggleCheckBox(this);
-        });
+        };
+        
+        // Handle Label Click
+        var checkedMarkedLabel = $(".checkedMarkedlabel");
+        checkedMarkedLabel.unbind("click", labelClick);
+        checkedMarkedLabel.bind("click", labelClick);
+
+        // Handle Actual Checkbox Click
+        var checkedMarked = $(".checkMarked");
+        checkedMarked.unbind("click", checkedMarkedClick);
+        checkedMarked.bind("click", checkedMarkedClick);        
 
         return this;
     };
