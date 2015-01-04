100421df73f9d3bf38396900a2058603e8e4f3bb
Blake Buck <blake@blakebuckit.com>
2015-01-03 22:23:58 -0500

    Major overhaul. Prettier checkbox and no images!

--- checkedMarked.js
+++ checkedMarked.js
@@ -1,61 +1,67 @@
 (function ($) {
-    var toggleCheckBox = function (chkbox) {
-        // Toggle checkmark image
-        $(chkbox).toggleClass("checked");
-        // Toggle checkbox checked status.
-        $(chkbox).find("input[type='checkbox']").prop('checked', function (i, value) {
-            return !value;
-        });
-    };
-
     $.fn.checkedMarked = function () {
+        var toggleCheckBox = function (chkbox) {
+            // Toggle checkmark
+            $(chkbox).toggleClass("checked");
 
-        // Go through each checkbox individually.
-        this.filter( "input[type='checkbox']" ).each(function(){
-            // Get checkbox's parent (used to tell if chkbox is inside label)           
+            // Toggle checkbox checked status.
+            $(chkbox).find("input[type='checkbox']").prop('checked', function (i, value) {
+                return !value;
+            });
+        };
+
+        return this.each(function() {
+
+            // Determine if parent is a label
             var chkboxParent = $(this).parent();
+            if (chkboxParent[0].nodeName.toLowerCase() == "label"){
 
-            // Check to see if we are inside of a label.
-            if (chkboxParent[0].nodeName.toLowerCase() == "label") {
-                // The parent element is a label
+                // Wrap label text in span
+                $(chkboxParent).contents().filter(function() {
+                    var re = /\S/;
+                    return this.nodeType === 3 && re.test(this.nodeValue);
+                })
+                    .wrap("<span class=\"checkedMarkedlabelTxt\"></span>");
+
+                // Give parent label a class name
                 $(chkboxParent).addClass("checkedMarkedlabel");
-            }            
-            if ($(this).parent().hasClass("checkMarked")){                
-                $(this).unwrap("<span></span>");
             }
-            
+
             // Create new checkbox and hide old one
             $(this).wrap("<span class='checkMarked'></span>");
             $(".checkMarked input[type='checkbox']").addClass("sr-only");
-            
+
             // Check to see if checkbox is already checked
             if ($(this).attr("checked")) {
                 $(this).closest(".checkMarked").toggleClass("checked");
             }
+
+            // Handle label click
+            $(this).closest(".checkedMarkedlabel").click(function(){
+                event.preventDefault();
+                var chkbox = $(this).find(".checkMarked");
+                toggleCheckBox(chkbox);
+            });
+
+            // Handle new checkbox click
+            $(this).closest(".checkMarked").click(function(){
+                event.preventDefault();
+                event.stopPropagation();
+                toggleCheckBox(this);
+            });
+
+            // Handle old checkbox change (Change due to tab + space)
+            $(this).keydown(function(e){
+                // Detect space (used to toggle checkboxes)
+                if(e.keyCode == 32){
+                    // Toggle checkbox (this will be undone by the toggleCheckBox function)
+                    $(this).prop('checked', function (i, value) {
+                        return !value;
+                    });
+                    var chkbox = $(this).find(".checkMarked");
+                    toggleCheckBox(chkbox);
+                }
+            });
         });
-
-        labelClick = function(event){
-            // Without this we would get double click
-            event.preventDefault();
-            var chkbox = $(this).find(".checkMarked");
-            toggleCheckBox(chkbox);
-        };
-        checkedMarkedClick = function(event){
-            event.preventDefault();
-            event.stopPropagation();
-            toggleCheckBox(this);
-        };
-        
-        // Handle Label Click
-        var checkedMarkedLabel = $(".checkedMarkedlabel");
-        checkedMarkedLabel.unbind("click", labelClick);
-        checkedMarkedLabel.bind("click", labelClick);
-
-        // Handle Actual Checkbox Click
-        var checkedMarked = $(".checkMarked");
-        checkedMarked.unbind("click", checkedMarkedClick);
-        checkedMarked.bind("click", checkedMarkedClick);        
-
-        return this;
     };
 })(jQuery);
